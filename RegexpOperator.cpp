1891d88702a994aaa1900a8fff63c21f7299610a
Carlos Brito <carlosdotb@gmail.com>
2017-03-22 16:47:15 -0600

    Add support for handling RegexpOperators

---
+++ RegexpOperator.cpp
@@ -0,0 +1,63 @@
+/*
+ * Author: Carlos Brito (carlos.brito524@gmail.com)
+ * Date: 3/21/17.
+ */
+
+#include "RegexpOperator.h"
+
+RegexpOperator::RegexpOperator(string name, char op, int precedence)
+        : op_(op),
+          precedence_(precedence),
+          name_(name)
+{
+}
+
+const int RegexpOperator::precedence() {
+    return precedence_;
+}
+
+const string RegexpOperator::name() {
+    return name_;
+}
+
+const char RegexpOperator::op() {
+    return op_;
+}
+
+bool RegexpOperator::operator<(RegexpOperator const& rhs) {
+    return this->precedence_ < rhs.precedence_;
+}
+
+bool RegexpOperator::operator>(RegexpOperator const &rhs) {
+    return this->precedence_ > rhs.precedence_;
+}
+
+bool RegexpOperator::operator>=(RegexpOperator const &rhs) {
+    return this->precedence_ >= rhs.precedence_;
+}
+
+bool RegexpOperator::operator<=(RegexpOperator const &rhs) {
+    return this->precedence_ <= rhs.precedence_;
+}
+
+ostream& operator<<(ostream& os,  RegexpOperator const& obj) {
+    return os << "RegexOperator( "<< obj.name_ << ", " << obj.op_ << " )";
+}
+
+RegexpInfixOperator::RegexpInfixOperator(string name, char op, bool left_assoc, int precedence)
+        : RegexpOperator(name, op, precedence),
+          left_assoc_(left_assoc) {
+}
+
+const bool RegexpInfixOperator::isLeftAssociative() {
+    return left_assoc_;
+}
+
+RegexpPostfixOperator::RegexpPostfixOperator(string name, char op, int precedence, bool assoc)
+        : RegexpOperator(name, op, precedence),
+          assoc_(assoc) {
+}
+
+const bool RegexpPostfixOperator::isAssociative() {
+    return assoc_;
+}
---
+++ RegexpOperator.h
@@ -0,0 +1,80 @@
+//<editor-fold desc="License">
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Copyright (C) 3/22/17 Carlos Brito
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.*
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+//</editor-fold>
+
+//<editor-fold desc="Description">
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Author: Carlos Brito (carlos.brito524@gmail.com)
+ * Date: 3/22/17.
+ *
+ * Description:
+ *
+ * TODO:
+ *
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*/
+//</editor-fold>
+
+#ifndef MYREGEX_REGEXPOPERATOR_H
+#define MYREGEX_REGEXPOPERATOR_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class RegexpOperator {
+    const char op_;
+    const string name_;
+    const int precedence_;
+
+public:
+    RegexpOperator(string name, char op, int precedence);
+
+    const int precedence();
+    const string name();
+    const char op();
+
+    bool operator<(RegexpOperator const& rhs);
+    bool operator>(RegexpOperator const& rhs);
+    bool operator>=(RegexpOperator const& rhs);
+    bool operator<=(RegexpOperator const& rhs);
+
+    friend ostream& operator<<(ostream& os, RegexpOperator const& obj);
+};
+
+class RegexpInfixOperator : public RegexpOperator {
+    const bool left_assoc_; // defines if operator is left associative
+
+public:
+    RegexpInfixOperator(string name, char op, bool left_assoc, int precedence);
+
+    const bool isLeftAssociative();
+};
+
+class RegexpPostfixOperator : public RegexpOperator {
+    const bool assoc_;
+public:
+    RegexpPostfixOperator(string name, char op, int precedence, bool assoc);
+
+    const bool isAssociative();
+};
+
+
+#endif //MYREGEX_REGEXPOPERATOR_H
