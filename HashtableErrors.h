d4d13d256db25b0adbcf7db0c245912e06f5c282
Carlos Brito <carlosdotb@gmail.com>
2017-03-22 12:33:56 -0600

    Add HashtableErrors header file which will contain the exceptions that are needed for the Hashtable to function

---
+++ HashtableErrors.h
@@ -0,0 +1,36 @@
+//<editor-fold desc="License">
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Copyright (C) 3/22/17 Carlos Brito
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.*
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+//</editor-fold>
+
+//<editor-fold desc="Description">
+/*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* Author: Carlos Brito (carlos.brito524@gmail.com)
+* Date: 3/22/17.
+*
+* Description:
+*
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*/
+//</editor-fold>
+
+#ifndef MYREGEX_HASHTABLEERRORS_H
+#define MYREGEX_HASHTABLEERRORS_H
+
+#endif //MYREGEX_HASHTABLEERRORS_H
