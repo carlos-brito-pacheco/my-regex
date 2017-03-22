30ac2f90a150a7644b42b17d949ea2364beb2bd2
Carlos Brito <carlosdotb@gmail.com>
2017-03-22 12:40:20 -0600

    Complete a lot of the functionality for Pearsonhashtable8

--- PearsonHashtable8.h
+++ PearsonHashtable8.h
@@ -1,6 +1,17 @@
 /*
+ * Copyright (C) 3/22/17 Carlos Brito
+ *
  * Author: Carlos Brito (carlos.brito524@gmail.com)
  * Date: 3/21/17.
+ *
+ * Description:
+ * This is an implementation of a Hashtable using Pearson hashing.
+ * This class represents a Hasthable of size 256.
+ *
+ * TODO:
+ * Implement collision handling
+ * Implement perfect hashing
+ * Check deletion of key in hashtable
  */
 
 #ifndef MYREGEX_PEARSONHASHTABLE8_H
@@ -14,27 +25,30 @@
 #include "Hashtable.h"
 
 using namespace std;
-int myrandom(int i) { return rand()%i; }
 
 // - - - - - - - - CLASS DEFINITION  - - - - - - - - //
 
 template <class T>
 class PearsonHashtable8 : public Hashtable<T> {
+    static const size_t  HASH_TABLE_SIZE_ = 256;
     static const size_t LOOKUP_TABLE_SIZE_ = 256;
 
+    vector<T*> *table_;
     vector<unsigned char> *lookup_table_;
 
 
 public:
     PearsonHashtable8();
 
-    int add(string key, T entry);
+    int add(string key, T const& entry);
     T get(string key);
     bool containsKey(string key);
+    void remove(string key);
 
 private:
     int hashfunc(string key);
     void initLookupTable();
+    void initHashTable();
 };
 
 
@@ -44,34 +58,48 @@ private:
 // constructs a hashtable with size 256
 template <class T>
 PearsonHashtable8<T>::PearsonHashtable8()
-        : Hashtable<T>(LOOKUP_TABLE_SIZE_) {
-
+        : Hashtable<T>(HASH_TABLE_SIZE_) {
 
     initLookupTable();
+    initHashTable();
 }
 
 // ------------------------
 // adds an entry of type T into hashtable
+// returns the hashkey of the new entry
 template <class T>
-int PearsonHashtable8<T>::add(string key, T entry) {
+int PearsonHashtable8<T>::add(string key, T const& entry) {
 
+    int hash = hashfunc(key);
+    table_->at(hash) = new T(entry);
 
-
-    return 0;
+    return hash;
 }
 
 // -----------------------
 // gets item of type T from hashtable
 template <class T>
 T PearsonHashtable8<T>::get(string key) {
-    return NULL;
+    int hash = hashfunc(key);
+    return *(table_->at(hash));
+}
+
+// -------------------------
+// removes specified key from table
+template <class T>
+void PearsonHashtable8<T>::remove(string key) {
+    int hash = hashfunc(key);
+    T *entry = table_->at(hash);
+    table_->at(hash) = NULL; // is this whole sequence correct? must check
+    delete entry;
 }
 
 // -----------------------
 // returns true if the specified key is in the hashtable
 template <class T>
 bool PearsonHashtable8<T>::containsKey(string key) {
-    return false;
+    int hash = hashfunc(key);
+    return table_->at(hash) != NULL;
 }
 
 // ----------------------
@@ -86,8 +114,6 @@ int PearsonHashtable8<T>::hashfunc(string key) {
      *
      * THIS IS NOT a perfect hashing implementation.
      *
-     * TODO:
-     * Implement perfect hashing.
      */
     unsigned char hash = (unsigned char) (key.length() % (LOOKUP_TABLE_SIZE_ - 1));
     for (string::iterator it = key.begin(); it != key.end(); it++)
@@ -95,12 +121,12 @@ int PearsonHashtable8<T>::hashfunc(string key) {
         hash = lookup_table_->at( hash ^ (*it) ) % (unsigned char) (LOOKUP_TABLE_SIZE_ - 1);
     }
 
-    return hash;
+    return (int) hash;
 }
 
 // ------------------------
 // initializes the lookup table to be used
-template<class T>
+template <class T>
 void PearsonHashtable8<T>::initLookupTable() {
 
     // Seed
@@ -113,14 +139,19 @@ void PearsonHashtable8<T>::initLookupTable() {
     int i = 0;
     for (vector<unsigned char>::iterator it = lookup_table_->begin(); it != lookup_table_->end(); it++)
     {
-
         *it = i++;
     }
 
     // Shuffle array
-    random_shuffle(lookup_table_->begin(), lookup_table_->end(), myrandom);
+    random_shuffle(lookup_table_->begin(), lookup_table_->end());
 }
 
-
+// ----------------------
+// initializes the hashtable
+template <class T>
+void PearsonHashtable8<T>::initHashTable() {
+    // Allocate space for table
+    table_ = new vector<T*>(HASH_TABLE_SIZE_);
+}
 
 #endif //MYREGEX_PEARSONHASHTABLE8_H
