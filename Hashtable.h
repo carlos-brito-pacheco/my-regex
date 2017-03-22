6f5375e58cb02674bfd3c390d1d3956acce7c743
Carlos Brito <carlosdotb@gmail.com>
2017-03-22 00:16:16 -0600

    Add description

--- Hashtable.h
+++ Hashtable.h
@@ -1,6 +1,11 @@
 /*
  * Author: Carlos Brito (carlos.brito524@gmail.com)
  * Date: 3/21/17.
+ *
+ * Description:
+ * This is an abstract class to define the behaviour
+ * of a basic Hashtable.
+ *
  */
 
 #ifndef MYREGEX_HASHTABLE_H
@@ -15,15 +20,11 @@ using  namespace std;
 template <class T>
 class Hashtable {
 
-    vector<list<T> > table_;
+    vector<list<T> > *table_;
     const size_t size_;
 
 public:
-    Hashtable(size_t size)
-            : size_(size)
-    {
-        table_.reserve(size);
-    }
+    Hashtable(size_t size);
 
     size_t size() const {
         return size_;
@@ -31,7 +32,14 @@ public:
 
     virtual int add(string key, T entry)= 0;
     virtual T get(string key)= 0;
-    virtual bool containsKey(string key);
+    virtual bool containsKey(string key)= 0;
 };
+
+template <class T>
+Hashtable<T>::Hashtable(size_t size)
+        : size_(size) {
+    table_ = new vector<list < T> > (size_);
+}
+
 
 #endif //MYREGEX_HASHTABLE_H
