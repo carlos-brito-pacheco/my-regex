d056b6ff01286b80291b2f6a551f5f5a4847751e
Carlos Brito <carlosdotb@gmail.com>
2017-03-23 10:26:06 -0600

    Add RegexpParser and RegexpToken classes to begin implementing syntax parsing on regular expressions

---
+++ RegexpToken.h
@@ -0,0 +1,46 @@
+//<editor-fold desc="Preamble">
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Copyright (C) 3/23/17 Carlos Brito
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.*
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+//</editor-fold>
+
+//<editor-fold desc="Description">
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Author: Carlos Brito (carlos.brito524@gmail.com)
+ * Date: 3/22/17.
+ *
+ * Description:
+ *
+ * TODO:
+ * 
+ * 
+ * 
+ * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+//</editor-fold>
+
+#ifndef MYREGEX_REGEXPTOKEN_H
+#define MYREGEX_REGEXPTOKEN_H
+
+
+class RegexpToken {
+
+};
+
+
+#endif //MYREGEX_REGEXPTOKEN_H
