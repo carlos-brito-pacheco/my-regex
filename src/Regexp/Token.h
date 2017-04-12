1311e3356fac34235dce370940e0a97f71a8abae
Carlos Brito <carlosdotb@gmail.com>
2017-04-12 03:01:54 -0500

    Update doc``

--- src/Regexp/Token.h
+++ src/Regexp/Token.h
@@ -18,23 +18,15 @@
 //</editor-fold>
 
 //<editor-fold desc="Description">
-/*
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Author: Carlos Brito (carlos.brito524@gmail.com)
- * Date: 3/22/17.
- *
- * Description:
- * This is a template class that manipulates "Token" types
- * during parsing. An object of type Token stores the lexeme
- * and has an attribute tag to indicate which "class" the token
- * belongs to. A representation could be this:
- *
- * Token token(relop, "<=");
+/**
+ * @file Token.h
+ * @brief Header file for the class Token
+ * @author Carlos Brito (carlos.brito524@gmail.com)
+ * @date: 3/22/17.
  *
  * TODO:
+ * Nothing for the moment.
  *
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 //</editor-fold>
 
@@ -46,27 +38,60 @@
 
 namespace Regexp {
 
+    /**
+     * @brief Token class to represent a Token during lexing of the Regular Expression grammar
+     *
+     * # Description
+     * This is a template class that manipulates "Token" types
+     * during parsing. An object of type Token stores the lexeme
+     * and has an attribute tag to indicate which "class" the token
+     * belongs to. A representation could be this:
+     *
+     * Token < relop, "<=" >
+     */
     class Token {
     public:
         // TAG CLASS
+        /**
+         * @brief Class to represent the different token classes
+         */
         class Tag {
-            int id_; // unique id to identify the which class a token belongs to (this is the id of the tag, not the token)
+            /// Unique id to identify the which class a token belongs to (this is the id of the tag, not the token)
+            int id_;
+
+            /// Name of class (i.e. "RELOP")
             std::string name_;
 
         public:
 
+            /**
+             * @brief Empty constructor
+             */
             Tag()
             {}
 
+            /**
+             * Constructor to instantiate a tag for a token
+             * @param id Id of token class
+             * @param name Name of class
+             */
             Tag(int id, std::string name)
                     : id_(id),
                       name_(name)
             {}
 
+            /**
+             * @brief Returns the id of tag
+             * @return Id of tag
+             */
             int id() const{
                 return id_;
             }
 
+            /**
+             * @brief Returns the name of the tag
+             * @return Name of tag
+             */
             std::string name() const {
                 return name_;
             }
@@ -91,6 +116,12 @@ namespace Regexp {
                 return os << obj.name();
             }
 
+            /**
+             * @brief Overloaded assignment operator
+             *
+             * @param rhs Right hand side
+             * @return Reference to new Tag
+             */
             Tag& operator=(Tag const& rhs) {
                 if (this != &rhs)
                 {
@@ -102,44 +133,68 @@ namespace Regexp {
         };
 
     private:
+        /// Tag for token
         Tag tag_;
+
+        /// Lexeme
         std::string lexeme_;
+
+        /// Length of lexem
         size_t length_;
 
     public:
 
+
+        /**
+         * @brief Empty constructor for Token
+         */
         Token()
         {}
 
+        /**
+         * Constructor to instatiate a Token with its tag and lexeme
+         * @param tag Corresponding tag for the token
+         * @param l Lexeme
+         */
         Token(Tag tag, std::string l)
                 : tag_(tag),
                   lexeme_(l),
                   length_(l.length())
         {}
 
-        // Returns tag of token
+        /**
+         * @brief Returns tag of token
+         * @return Token tag
+         */
         Tag tag() const {
             return tag_;
         }
 
-        // Returns lexeme of token
+        /**
+         * @brief Returns associated lexeme of token
+         * @return Lexeme
+         */
         std::string lexeme() {
             return lexeme_;
         }
 
-        // Returns length of token
+        /**
+         * @brief Returns length of lexeme
+         * @return Length of lexeme
+         */
         size_t length() {
             return length_;
-        }
-
-        char first() {
-            return lexeme_[0];
         }
 
         friend std::ostream& operator<<(std::ostream& os, Token const& obj) {
             return os <<  "Token<" << obj.tag() << ", " << obj.lexeme_ << ">";
         }
 
+        /**
+         * @brief Overloaded assignment operator
+         * @param rhs Right hand side
+         * @return Reference to new token
+         */
         Token& operator=(Token const& rhs) {
             if (this != &rhs)
             {
